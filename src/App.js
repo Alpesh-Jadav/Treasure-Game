6738b074ac2f913350d6e1a8d32072ad0088a68b
Alpesh-Jadav <alpeshjadav11899@gmail.com>
2020-08-18 04:40:08 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import World from './features/world'
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+          <World/>
+      </div>
+    )
+  }
+}
---
+++ src/features/map/index.js
@@ -0,0 +1,68 @@
+
+import React from 'react'
+import { connect } from 'react-redux'
+import { SPRITE_SIZE } from '../../config/constants'
+import './styles.css'
+
+function getTileSprite(type) {
+    switch(type) {
+
+        case 0:
+            return 'grass'
+        case 3:
+            return 'tree'
+        case 4:
+            return 'chest'
+        case 5:
+            return 'rock'
+        case 6:
+            return 'tree'
+        default: 
+    }
+}
+function MapTile(props) {
+    return <div 
+        className={`tile ${getTileSprite(props.tile)}`}
+        style={{
+            height: SPRITE_SIZE,
+            width: SPRITE_SIZE,
+        }}
+    />
+}
+function MapRow(props) {
+    return <div className="row"
+        style={{
+            height: SPRITE_SIZE,
+            
+        }}
+    >
+        {
+            props.tiles.map( (tile,i) => <MapTile key={i} tile={tile} />)
+        }
+    </div>
+}
+ function Map(props) {
+    return (
+        <div
+            style ={{
+                position: 'relative',
+                top: '0px',
+                left: '0px',
+                width: '800px',
+                height: '480px',
+                border: '4px solid white',
+                
+            }}
+         >
+             {
+                 props.tiles.map((row,i) => <MapRow key={i} tiles={row} />)
+             }
+         </div>
+    )
+}
+function mapStateToProps(state) {
+    return {
+        tiles: state.map.tiles,
+    }
+}
+export default connect(mapStateToProps)(Map)
